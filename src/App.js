04a8f3bfd462276c4d2fc57bb71313cf7b2ae8a2
alfred <alfred@gmail.com>
2023-07-14 17:37:19 +0200

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,107 +1,90 @@
 import React, { useState, useEffect } from 'react';
 import './App.css';
 import dbData from './db.json';
-import Caro from './caro'; 
-import FavoriteMoviesPage from './FavoriteMoviesPage'; 
-import WatchLaterPage from './WatchLaterPage'; 
-import SideMenu from './SideMenu'; 
+import Caro from './caro';
+import FavoriteMoviesPage from './FavoriteMoviesPage';
+import WatchLaterPage from './WatchLaterPage';
+import SideMenu from './SideMenu';
 
 const App = () => {
   const [movies, setMovies] = useState([]); // State variable for movies
   const [searchTerm, setSearchTerm] = useState(''); // for search term
   const [favorites, setFavorites] = useState([]); // favorite movies
-  const [watchLaterList, setWatchLaterList] = useState([]); //watch later list
+  const [watchLaterList, setWatchLaterList] = useState([]); // watch later list
   const [showFavorites, setShowFavorites] = useState(false); // showing/hiding favorite movies
-  const [showWatchLater, setShowWatchLater] = useState(false); //showing/hiding watch later list
+  const [showWatchLater, setShowWatchLater] = useState(false); // showing/hiding watch later list
   const [selectedGenre, setSelectedGenre] = useState(null); // selected genre
-  const [selectedDecade, setSelectedDecade] = useState(null); //  decade
+  const [selectedDecade, setSelectedDecade] = useState(null); // selected decade
 
   useEffect(() => {
-    setMovies(dbData.movies); // Fetching movie from JSON setting it in the movies state
+    setMovies(dbData.movies); // Fetching movies from JSON and setting them in the movies state
   }, []);
 
   const handleSearchChange = (event) => {
     setSearchTerm(event.target.value); // Updating the search term state based on user input
   };
-//Repeat 
+
   const filteredMovies = movies.filter((movie) => {
-    const includesSearchTerm = movie.title.toLowerCase().includes(searchTerm.toLowerCase()); //Checking if the movie title includes the search term
+    const includesSearchTerm = movie.title.toLowerCase().includes(searchTerm.toLowerCase()); // Checking if the movie title includes the search term
     const matchesGenre = selectedGenre ? movie.genres.includes(selectedGenre) : true; // Checking if the movie belongs to the selected genre
-    const matchesDecade = selectedDecade ? (
-      parseInt(movie.year) >= selectedDecade &&
-      parseInt(movie.year) < selectedDecade + 10
-    ) : true; 
+    const matchesDecade = selectedDecade ? parseInt(movie.year) >= selectedDecade && parseInt(movie.year) < selectedDecade + 10 : true; // Checking if the movie belongs to the selected decade
     return includesSearchTerm && matchesGenre && matchesDecade; // Returning movies that satisfy all the filter conditions
   });
 
-
   const addToFavorites = (movie) => {
-    setFavorites([...favorites, movie]); // Adding a movie to the favorites state
-  };//By using spread operator & creates an array with all the existing movies in the fav state
+    setFavorites((prevFavorites) => [...prevFavorites, movie]); // Adding a movie to the favorites state
+  };
+
   const addToWatchLaterList = (movie) => {
-    setWatchLaterList([...watchLaterList, movie]); //Movie is an object that represents a movies
-    // The funtion addToFavorites take the object and adds it to the favorites state
+    setWatchLaterList((prevList) => [...prevList, movie]); // Adding a movie to the watch later list state
   };
 
   const toggleFavorites = () => {
-    setShowFavorites(!showFavorites); // Toggling the showFavorites state to show or hide the favorite movies
-  };//this will update teh use state of teh fav movies and show the ifnroamtion in the screen
+    setShowFavorites((prevState) => !prevState); // Toggling the showFavorites state to show or hide the favorite movies
+  };
 
   const toggleWatchLater = () => {
-    setShowWatchLater(!showWatchLater); // Toggling the showWatchLater state to show or hide the watch later list
-  };//this will update teh use state of teh WTL movies and show the ifnroamtion in the screen
-
+    setShowWatchLater((prevState) => !prevState); // Toggling the showWatchLater state to show or hide the watch later list
+  };
 
   const handleGenreSelect = (genre) => {
     setSelectedGenre(genre); // Updating the selected genre state based on user selection
-  };//genre is an object that has data, the function gets the data, adn will add it to the genre state
+  };
 
   const handleDecadeSelect = (decade) => {
-    setSelectedDecade(decade);//object data from the funtion to state
-   };
-//Repeat & Learn
+    setSelectedDecade(decade); // Updating the selected decade state based on user selection
+  };
+
   const removeFromFavorites = (movie) => {
-    const updatedFavorites = favorites.filter((fav) => fav !== movie); // Removing a movie from the favorites state
-    setFavorites(updatedFavorites);
+    setFavorites((prevFavorites) => prevFavorites.filter((fav) => fav !== movie)); // Removing a movie from the favorites state
   };
 
   const removeFromWatchLaterList = (movie) => {
-    const updatedWatchLaterList = watchLaterList.filter((m) => m !== movie); // Removing a movie from the watch later list state
-    setWatchLaterList(updatedWatchLaterList);
+    setWatchLaterList((prevList) => prevList.filter((m) => m !== movie)); // Removing a movie from the watch later list state
   };
 
   return (
-    <div className='body'>
-      <SideMenu genres={dbData.genres} onSelectGenre={handleGenreSelect} onSelectDecade={handleDecadeSelect} /> {/* Rendering the SideMenu component with genres and event handlers */}
-      <div className='search-box'>
+    <div className="body">
+      <SideMenu genres={dbData.genres} onSelectGenre={handleGenreSelect} onSelectDecade={handleDecadeSelect} />
+      <div className="search-box">
         <input
           type="text"
           placeholder="Search movies"
           value={searchTerm}
           onChange={handleSearchChange}
-        /> {/* Rendering the search input box */}
+        />
       </div>
 
       <div className="top-button">
-        <button
-          type="button"
-          className="fav-button"
-          onClick={toggleFavorites}
-        >
-          Favorites List
-        </button> {/* Button to toggle showing/hiding favorite movies */}
-        <button
-          className="later-button"
-          onClick={toggleWatchLater}
-          type="button"
-        >
-          WatchLater List
-        </button> {/* Button to toggle showing/hiding watch later list */}
+        <button type="button" className="fav-button" onClick={toggleFavorites}>
+        {showFavorites ? 'Hide Favorites List' : 'Favorites List'}
+        </button>
+        <button className="later-button" onClick={toggleWatchLater} type="button">
+        {showWatchLater ? 'Hide Watch Later List' : 'Watch Later List'}
+        </button>
       </div>
 
-      <div>
-        <Caro /> {/* Rendering the Caro component */}
-      </div>
+      
 
       {showFavorites ? (
         <FavoriteMoviesPage favorites={favorites} removeFromFavorites={removeFromFavorites} />
@@ -109,16 +92,15 @@ const App = () => {
         <WatchLaterPage watchLaterList={watchLaterList} removeFromWatchLaterList={removeFromWatchLaterList} />
       ) : (
         <div className="movie-container">
+   
+        <Caro />
+
           {filteredMovies.map((movie) => (
             <div className="movie-card" key={movie.id}>
               <img src={movie.posterUrl} alt={movie.title} className="movie-image" />
               <div className="movie-details">
                 <span className="movie-year">{movie.year}</span>
-                <button
-                  className="like-button"
-                  type="button"
-                  onClick={() => addToFavorites(movie)}
-                >
+                <button className="like-button" type="button" onClick={() => addToFavorites(movie)}>
                   Favorites
                 </button>
                 <button
--- src/SideMenu.js
+++ src/SideMenu.js
@@ -1,5 +1,7 @@
 import React, { useState } from 'react';
 import './sidemenu.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faHome, faFilm, faLaugh, faTheaterMasks, faHeart, faClock, faCalendar, faChevronLeft } from '@fortawesome/free-solid-svg-icons';
 
 const SideMenu = ({ genres, onSelectGenre, onSelectDecade }) => {
   const [isActive, setIsActive] = useState(false);
@@ -23,20 +25,20 @@ const SideMenu = ({ genres, onSelectGenre, onSelectDecade }) => {
   return (
     <div className={`sideMenu ${isActive ? 'active' : ''}`}>
       <div className={`menuIcon ${isActive ? 'active' : ''}`} onClick={toggleMenu}>
-        <span></span>
+      <span></span>
       </div>
       <ul>
-        <li><a href="#" className='boldo' onClick={handleHomeSelect}>Home</a></li>
-        <li><a href="#" className='compo' onClick={() => handleGenreSelect('Action')}>Action</a></li>
-        <li><a href="#" className='compo' onClick={() => handleGenreSelect('Comedy')}>Comedy</a></li>
-        <li><a href="#" className='compo' onClick={() => handleGenreSelect('Drama')}>Drama</a></li>
-        <li><a href="#" className='compo' onClick={() => handleGenreSelect('Romance')}>Romance</a></li>
-        <li><a href="#" className='compo' onClick={() => handleGenreSelect('Horror')}>Horror</a></li>
-        <li><a href="#" className='boldo' onClick={() => handleGenreSelect(null)}>All Decades</a></li>
-        <li><a href="#" className='compo' onClick={() => handleDecadeSelect(1990)}>1990s</a></li>
-        <li><a href="#" className='compo' onClick={() => handleDecadeSelect(2000)}>2000s</a></li>
-        <li><a href="#" className='compo' onClick={() => handleDecadeSelect(2010)}>2010s</a></li>
-        <li><a href="#" className='compo' onClick={() => handleDecadeSelect(2020)}>2020s</a></li>
+        <li><a href="#" className='boldo' onClick={handleHomeSelect}><FontAwesomeIcon icon={faHome} /> Home</a></li>
+        <li><a href="#" className='compo' onClick={() => handleGenreSelect('Action')}><FontAwesomeIcon icon={faFilm} /> Action</a></li>
+        <li><a href="#" className='compo' onClick={() => handleGenreSelect('Comedy')}><FontAwesomeIcon icon={faLaugh} /> Comedy</a></li>
+        <li><a href="#" className='compo' onClick={() => handleGenreSelect('Drama')}><FontAwesomeIcon icon={faTheaterMasks} /> Drama</a></li>
+        <li><a href="#" className='compo' onClick={() => handleGenreSelect('Romance')}><FontAwesomeIcon icon={faHeart} /> Romance</a></li>
+        <li><a href="#" className='compo' onClick={() => handleGenreSelect('Horror')}><FontAwesomeIcon icon={faFilm} /> Horror</a></li>
+        <li><a href="#" className='boldo' onClick={() => handleGenreSelect(null)}><FontAwesomeIcon icon={faClock} /> All Decades</a></li>
+        <li><a href="#" className='compo' onClick={() => handleDecadeSelect(1990)}><FontAwesomeIcon icon={faCalendar} /> 1990s</a></li>
+        <li><a href="#" className='compo' onClick={() => handleDecadeSelect(2000)}><FontAwesomeIcon icon={faCalendar} /> 2000s</a></li>
+        <li><a href="#" className='compo' onClick={() => handleDecadeSelect(2010)}><FontAwesomeIcon icon={faCalendar} /> 2010s</a></li>
+        <li><a href="#" className='compo' onClick={() => handleDecadeSelect(2020)}><FontAwesomeIcon icon={faCalendar} /> 2020s</a></li>
       </ul>
     </div>
   );
