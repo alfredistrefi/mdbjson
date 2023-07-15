14cbfca19281f12cd63bea8b3da7b5fac95d2285
Alfred <100093127+alfredistrefi@users.noreply.github.com>
2023-07-15 22:47:10 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,67 +5,98 @@ import Caro from './caro';
 import FavoriteMoviesPage from './FavoriteMoviesPage';
 import WatchLaterPage from './WatchLaterPage';
 import SideMenu from './SideMenu';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {
+  faHome,
+  faFilm,
+  faLaugh,
+  faTheaterMasks,
+  faHeart,
+  faClock,
+  faCalendar,
+  faChevronLeft,
+} from '@fortawesome/free-solid-svg-icons';
+import GenreSelection from './GenreSelection';
 
 const App = () => {
-  const [movies, setMovies] = useState([]); // State variable for movies
-  const [searchTerm, setSearchTerm] = useState(''); // for search term
-  const [favorites, setFavorites] = useState([]); // favorite movies
-  const [watchLaterList, setWatchLaterList] = useState([]); // watch later list
-  const [showFavorites, setShowFavorites] = useState(false); // showing/hiding favorite movies
-  const [showWatchLater, setShowWatchLater] = useState(false); // showing/hiding watch later list
-  const [selectedGenre, setSelectedGenre] = useState(null); // selected genre
-  const [selectedDecade, setSelectedDecade] = useState(null); // selected decade
+  const [movies, setMovies] = useState([]);
+  const [searchTerm, setSearchTerm] = useState('');
+  const [favorites, setFavorites] = useState([]);
+  const [watchLaterList, setWatchLaterList] = useState([]);
+  const [showFavorites, setShowFavorites] = useState(false);
+  const [showWatchLater, setShowWatchLater] = useState(false);
+  const [selectedGenre, setSelectedGenre] = useState(null);
+  const [selectedDecade, setSelectedDecade] = useState(null);
 
   useEffect(() => {
-    setMovies(dbData.movies); // Fetching movies from JSON and setting them in the movies state
+    setMovies(dbData.movies);
   }, []);
 
   const handleSearchChange = (event) => {
-    setSearchTerm(event.target.value); // Updating the search term state based on user input
+    setSearchTerm(event.target.value);
   };
 
   const filteredMovies = movies.filter((movie) => {
-    const includesSearchTerm = movie.title.toLowerCase().includes(searchTerm.toLowerCase()); // Checking if the movie title includes the search term
-    const matchesGenre = selectedGenre ? movie.genres.includes(selectedGenre) : true; // Checking if the movie belongs to the selected genre
-    const matchesDecade = selectedDecade ? parseInt(movie.year) >= selectedDecade && parseInt(movie.year) < selectedDecade + 10 : true; // Checking if the movie belongs to the selected decade
-    return includesSearchTerm && matchesGenre && matchesDecade; // Returning movies that satisfy all the filter conditions
+    const includesSearchTerm = movie.title.toLowerCase().includes(searchTerm.toLowerCase());
+    const matchesGenre = selectedGenre ? movie.genres.includes(selectedGenre) : true;
+    const matchesDecade = selectedDecade
+      ? parseInt(movie.year) >= selectedDecade && parseInt(movie.year) < selectedDecade + 10
+      : true;
+    return includesSearchTerm && matchesGenre && matchesDecade;
   });
 
   const addToFavorites = (movie) => {
-    setFavorites((prevFavorites) => [...prevFavorites, movie]); // Adding a movie to the favorites state
+    setFavorites((prevFavorites) => [...prevFavorites, movie]);
   };
 
   const addToWatchLaterList = (movie) => {
-    setWatchLaterList((prevList) => [...prevList, movie]); // Adding a movie to the watch later list state
+    setWatchLaterList((prevList) => [...prevList, movie]);
   };
 
   const toggleFavorites = () => {
-    setShowFavorites((prevState) => !prevState); // Toggling the showFavorites state to show or hide the favorite movies
+    setShowFavorites((prevState) => !prevState);
+    setShowWatchLater(false);
   };
 
   const toggleWatchLater = () => {
-    setShowWatchLater((prevState) => !prevState); // Toggling the showWatchLater state to show or hide the watch later list
+    setShowWatchLater((prevState) => !prevState);
+    setShowFavorites(false);
   };
 
   const handleGenreSelect = (genre) => {
-    setSelectedGenre(genre); // Updating the selected genre state based on user selection
+    setSelectedGenre(genre);
   };
 
   const handleDecadeSelect = (decade) => {
-    setSelectedDecade(decade); // Updating the selected decade state based on user selection
+    setSelectedDecade(decade);
   };
 
   const removeFromFavorites = (movie) => {
-    setFavorites((prevFavorites) => prevFavorites.filter((fav) => fav !== movie)); // Removing a movie from the favorites state
+    setFavorites((prevFavorites) => prevFavorites.filter((fav) => fav !== movie));
   };
 
   const removeFromWatchLaterList = (movie) => {
-    setWatchLaterList((prevList) => prevList.filter((m) => m !== movie)); // Removing a movie from the watch later list state
+    setWatchLaterList((prevList) => prevList.filter((m) => m !== movie));
+  };
+
+  const reloadPage = () => {
+    // Reload the page
+    window.location.reload();
   };
 
   return (
     <div className="body">
-      <SideMenu genres={dbData.genres} onSelectGenre={handleGenreSelect} onSelectDecade={handleDecadeSelect} />
+    <div className='Space'>
+    <SideMenu genres={dbData.genres} onSelectDecade={handleDecadeSelect} />
+    </div>
+    <div className="Space2">
+    <div className="container-btn">
+        <div className="home-button" onClick={reloadPage}>
+          <FontAwesomeIcon icon={faHome} />
+        </div>
+      </div>
+    </div>
+     
       <div className="search-box">
         <input
           type="text"
@@ -76,15 +107,27 @@ const App = () => {
       </div>
 
       <div className="top-button">
-        <button type="button" className="fav-button" onClick={toggleFavorites}>
-        {showFavorites ? 'Hide Favorites List' : 'Favorites List'}
-        </button>
-        <button className="later-button" onClick={toggleWatchLater} type="button">
-        {showWatchLater ? 'Hide Watch Later List' : 'Watch Later List'}
-        </button>
+        {showFavorites ? (
+          <button type="button" className="fav-button" onClick={toggleFavorites}>
+            Hide Favorites List
+          </button>
+        ) : (
+          <button type="button" className="fav-button" onClick={toggleFavorites}>
+            Favorites List
+          </button>
+        )}
+        {showWatchLater ? (
+          <button className="later-button" onClick={toggleWatchLater} type="button">
+            Hide Watch Later List
+          </button>
+        ) : (
+          <button className="later-button" onClick={toggleWatchLater} type="button">
+            Watch Later List
+          </button>
+        )}
       </div>
 
-      
+      <GenreSelection handleGenreSelect={handleGenreSelect} />
 
       {showFavorites ? (
         <FavoriteMoviesPage favorites={favorites} removeFromFavorites={removeFromFavorites} />
@@ -92,9 +135,7 @@ const App = () => {
         <WatchLaterPage watchLaterList={watchLaterList} removeFromWatchLaterList={removeFromWatchLaterList} />
       ) : (
         <div className="movie-container">
-   
-        <Caro />
-
+          <Caro />
           {filteredMovies.map((movie) => (
             <div className="movie-card" key={movie.id}>
               <img src={movie.posterUrl} alt={movie.title} className="movie-image" />
